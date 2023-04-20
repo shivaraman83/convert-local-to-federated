4d022a79b24c57838168e4c53eaed27b82e3edf1
Siva <shivaraman83@gmail.com>
2023-04-20 10:04:41 -0400

    added

---
+++ fed_create_yaml.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+SOURCE_JPD_URL="${1:?please enter JPD URL. ex - https://ramkannans-sbx.dev.gcp.devopsacc.team}"
+TARGET_JPD_URL="${2:?please enter JPD URL. ex - https://ramkannans-apac-sbx.dev.gcp.devopsacc.team}"
+USER_NAME="${3:?please provide the username in JPD . ex - admin}"  ### common credentials across 3 JPD's
+USER_TOKEN="${4:?please provide the user pwd or token or API Key . ex - password}"  ### common credentials across 3 JPD's
+
+./getRepoList.sh "$SOURCE_JPD_URL" federated "$USER_NAME" "$USER_TOKEN"
+
+FILE_NAME="fed_repo_list.yaml"
+
+rm -rf $FILE_NAME
+
+echo "federatedRepositories:" > $FILE_NAME
+
+while IFS= read -r reponame; do
+    echo -e "  ${reponame}:" >> $FILE_NAME
+    echo -e "    federatedMembers:" >> $FILE_NAME 
+    echo -e "      - url: ${SOURCE_JPD_URL}/artifactory/${reponame}" >> $FILE_NAME
+    echo -e "        enabled: true" >> $FILE_NAME
+    echo -e "      - url: ${TARGET_JPD_URL}/artifactory/${reponame}" >> $FILE_NAME
+    echo -e "        enabled: true" >> $FILE_NAME
+done < repos_list_federated.txt 
+
+### sample cmd to run - ./fed_create_yaml.sh https://ramkannans-sbx.dev.gcp.devopsacc.team https://ramkannans-apac-sbx.dev.gcp.devopsacc.team admin ****
---
+++ fed_repo_member_sync.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+### Exit the script on any failures
+set -eo pipefail
+set -e
+set -u
+
+### Get Arguments
+SOURCE_JPD_URL="${1:?please enter JPD URL. ex - https://ramkannan.jfrog.io}"
+USER_NAME="${2:?please provide the username in JPD . ex - admin}"
+JPD_AUTH_TOKEN="${3:?please provide the user identity token}"
+
+curl -X PATCH -H 'Content-Type: application/yaml' -u "${USER_NAME}":"${JPD_AUTH_TOKEN}" "${SOURCE_JPD_URL}/artifactory/api/system/configuration" -T fed_repo_list.yaml
+
+##jf rt curl -XPATCH /api/system/configuration -T fed_repo_list.yaml
+yq e fed_repo_list.yaml -o=j -I=0 |  jq -r '.[]|keys[]' |
+while read -r repo_id; do
+  echo "\nSync for REPO ID ==> $repo_id"
+  #jf rt curl -XPOST /api/federation/configSync/$repo_id
+  curl -X POST -H 'Content-Type: application/yaml' -u "${USER_NAME}":"${JPD_AUTH_TOKEN}" "${SOURCE_JPD_URL}/artifactory/api/federation/configSync/$repo_id"
+done
