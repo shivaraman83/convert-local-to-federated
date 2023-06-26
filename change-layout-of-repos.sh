273a97b8b0aea377a9497c463a71f5bc5888848d
Siva <shivaraman83@gmail.com>
2023-06-26 17:37:20 -0400

    updated

---
+++ change-layout-of-repos.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+# JFrog hereby grants you a non-exclusive, non-transferable, non-distributable right
+# to use this  code   solely in connection with your use of a JFrog product or service.
+# This  code is provided 'as-is' and without any warranties or conditions, either
+# express or implied including, without limitation, any warranties or conditions of
+# title, non-infringement, merchantability or fitness for a particular cause.
+# Nothing herein shall convey to you any right or title in the code, other than
+# for the limited use right set forth herein. For the purposes hereof "you" shall
+# mean you as an individual as well as the organization on behalf of which you
+# are using the software and the JFrog product or service.
+
+### Exit the script on any failures
+## define variable
+cd repository
+rm *.*
+jf c use ukg-saas
+jf rt curl api/repositories\?type=local\&packageType=npm -s | jq -rc '.[] | .key' > repositories.list
+
+cat repositories.list |  while read line
+do
+     REPO=$(echo $line | cut -d ':' -f 2)
+     echo "Getting configuration for "$REPO
+     if [[ ${excluderepos[*]} =~ $REPO ]]; then
+        echo "Skipping excluded Repo" $REPO
+     else
+        jf rt curl api/repositories/$REPO >> $REPO-config.json
+        tempfile=$(mktemp -u)
+        jq --arg sub "npm-default" '.repoLayoutRef|= $sub' "$REPO-config.json" > "$tempfile"
+        mv "$tempfile" "$REPO-config.json"
+        #data=$( jf rt curl  -X POST api/repositories/$REPO -H "Content-Type: application/json" -T $REPO-config.json --server-id=ukg-saas -s | grep message | xargs)
+        #echo $data
+        #if [[ $data == *"message"*  ]];then
+        #    echo "$REPO" >> conflicting-repos.txt
+        #fi
+     fi
+done
